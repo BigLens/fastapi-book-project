825480b907d3b4721aed2f71bd5253208c44df28
Biglens <adedejibioku@gmail.com>
2025-02-12 23:47:35 +0100

    testing out CD pipeline behaviour

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,7 +47,7 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
-
+#get book by id
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int) -> Book:
     book = db.books.get(book_id)
