de41d8f14797c6241244c0aa71be8be98232a77b
Biglens <adedejibioku@gmail.com>
2025-02-13 23:38:23 +0100

    my cd didn't work so i tried another approach

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,7 +47,7 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
-#get the book by id endpoint
+#get the book by id endpoints
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int) -> Book:
     book = db.books.get(book_id)
